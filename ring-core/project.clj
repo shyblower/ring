ec88cf467cb50af17e07eddf3166c032c5f2f896
Mark McGranaghan <mmcgrana@gmail.com>
2010-10-25 08:44:54 -0700

    Depend on Clojure 1.3 alpha2.

--- ring-core/project.clj
+++ ring-core/project.clj
@@ -1,8 +1,7 @@
 (defproject ring/ring-core "0.3.2"
   :description "Ring core libraries."
   :url "http://github.com/mmcgrana/ring"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-alpha2"]
                  [commons-codec "1.4"]
                  [commons-io "1.4"]
                  [commons-fileupload "1.2.1"]
