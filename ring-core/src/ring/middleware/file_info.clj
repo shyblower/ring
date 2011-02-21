d025022a3920ad31ab47737636a7b21ebda191e4
Thomas Scheiblauer <tom@sharkbay.at>
2011-02-21 19:09:29 +0100

    added 'wrap-contenttype-by-uriext' middleware to file_info.clj which sets the Content-Type header of the response to the mime-type determined by the extension of the request uri

--- ring-core/src/ring/middleware/file_info.clj
+++ ring-core/src/ring/middleware/file_info.clj
@@ -63,14 +63,14 @@
 
 (defn- get-extension
   "Returns the file extension of a file."
-  [^File file]
-  (second (re-find #"\.([^./\\]+)$" (.getPath file))))
+  [path]
+  (second (re-find #"\.([^./\\]+)$" path)))
 
 (defn- guess-mime-type
   "Returns a String corresponding to the guessed mime type for the given file,
   or application/octet-stream if a type cannot be guessed."
-  [^File file mime-types]
-  (get mime-types (get-extension file) "application/octet-stream"))
+  [path mime-types]
+  (get mime-types (get-extension path) "application/octet-stream"))
 
 (defn make-http-format
   "Formats or parses dates into HTTP date format (RFC 822/1123)."
@@ -99,7 +99,7 @@
     (fn [req]
       (let [{:keys [headers body] :as response} (app req)]
         (if (instance? File body)
-          (let [file-type   (guess-mime-type body mime-types)
+          (let [file-type   (guess-mime-type (.getPath body) mime-types)
                 file-length (.length ^File body)
                 lmodified   (Date. (.lastModified ^File body))
                 response    (-> response
@@ -112,3 +112,13 @@
                            (assoc :body ""))
               (-> response (res/header "Content-Length" file-length))))
           response)))))
+
+(defn wrap-contenttype-by-uriext
+  "Wrap a ring handler to set the Content-Type header of the response
+  determined by the extension of the request uri"
+  [handler & [custom-mime-types]]
+  (let [mime-types (merge base-mime-types custom-mime-types)]
+    (fn [request]
+      (let [{:keys [uri]} request
+            {:keys [body] :as response} (handler request) ]
+        (res/content-type response (guess-mime-type uri mime-types)) ))))
